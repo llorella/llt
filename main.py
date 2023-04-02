ea7442d122e4501f31c3aae4270d0bfb254138d5
llorella <lucianolauro77@gmail.com>
2023-04-02 04:02:34 -0500

    add message submodule

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import os
 import sys
+import json
 
 sys.path.append(os.path.expanduser('~/message-history-tree'))
-from message import Message, save_message_history, load_message_history
+from message import Message
 
-def run_thread(model: str, filename: str, folder: str, prompts: list(str)) -> None:
-    if filename:
-        root_message = load_message_history(filename, folder)
-    else:
-        root_message = None
+from typing import List
+def save_message_history(root_message: Message, folder: str) -> None:
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    filename = input("Enter filename: ") + ".json"
+    path = os.path.join(folder, filename)
+    with open(path, 'w') as outfile:
+        json.dump(root_message.expand_iter(), outfile, indent=4)
+    print(f"Message tree saved to {filename}")
+    
+def load_message_history(filename: str, folder: str) -> Message:
+    if not filename: return None
+    path = os.path.join(folder, filename)
+    with open(path, 'r') as infile:
+        message_dicts = json.load(infile)
+    root_message = Message(message_dicts[0]['role'], message_dicts[0]['content'], None)
+    current_message = root_message
+    for message_dict in message_dicts[1:]:
+        current_message = Message(message_dict['role'], message_dict['content'], current_message)
+    return current_message
+
+def run_thread(model: str, folder: str, file: str, prompts: List[str]) -> None:
+    root_message = load_message_history(file, folder)
     
     #system denotes new instructions to assistant for consideration
     #each system message can be considered the start of a new iteration of chat history 
@@ -16,6 +35,8 @@ def run_thread(model: str, filename: str, folder: str, prompts: list(str)) -> No
 
     n = len(prompts)
     idx = 1
+    
+    
     while True: 
         if (idx < n): 
            user_input = prompts[idx]
@@ -26,7 +47,7 @@ def run_thread(model: str, filename: str, folder: str, prompts: list(str)) -> No
                 print("You entered:", user_input)
             except KeyboardInterrupt:
                 print("\nKeyboardInterrupt detected. Exiting...")
-                save_message_history(current_message, folder)
+                save_message_history(current_message, "histories")
                 break
             
         user_message = Message('user', user_input, current_message)
--- test.py
+++ test.py
@@ -6,17 +6,14 @@ model = "gpt-3.5-turbo"
 folder = "./sandbox/"
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Combine files from a directory or a list of filenames.")
-    parser.add_argument('-f', '--file', help="JSON file containing history of previous runs.")
+    parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--directory', help="Directory for loading and storing JSON history files.")
+    parser.add_argument('-f', '--file', help="JSON file containing history of previous runs.")
     parser.add_argument('-p', '--prompts', type=str, help="List of preset prompts.")
     args = parser.parse_args()
 
-    if ( args.file or args.prompts ):
-        prompts = args.prompts.split(",") if args.prompts else []
-        run_thread(model, args.file, folder, promptd)
-        
-    else: 
+    if not ( args.file or args.directory or args.prompts  ):
         raise parser.error("Either --history or --prompts must be provided.")
-
-    
+    else: 
+        prompts = args.prompts.split(",") if args.prompts else []
+        run_thread(model, args.directory, args.file, prompts)
