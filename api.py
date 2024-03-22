c3cb0abafcc13082ea0af728c5a6f9a980f7d7af
kylewescott <89892471+kylewescott@users.noreply.github.com>
2024-03-21 21:53:50 -0400

    Fix path concat for config dir

--- api.py
+++ api.py
@@ -14,7 +14,7 @@ def load_config(path: str):
     with open(path, 'r') as config_file:
         return yaml.safe_load(config_file)
 
-api_config = load_config(os.getenv('LLT_PATH') + "config.yaml")
+api_config = os.path.join(load_config(os.getenv('LLT_PATH'), "config.yaml"))
 
 #placeholder with limited functionality
 def count_tokens(message, model):
